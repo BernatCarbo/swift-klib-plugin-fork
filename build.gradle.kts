23a32f473c5d26a9e03961e1251764d4708bfdca
bernat <bernatgarciacarbo@gmail.com>
2025-07-03 22:13:40 +0200

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,15 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.gradle.publish) apply false
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
+            username = System.getenv("MAVEN_USERNAME")
+            password = System.getenv("MAVEN_TOKEN")
+        }
+    }
 }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java-gradle-plugin")
     alias(libs.plugins.kotlin.jvm)
     `maven-publish`
+    signing
 }
 
 dependencies {
@@ -25,7 +26,6 @@ kotlin {
     jvmToolchain(17)
 }
 
-/*
 @Suppress("UnstableApiUsage")
 gradlePlugin {
     website = "https://github.com/bernatcarbo/swift-klib-plugin-fork"
@@ -41,17 +41,58 @@ gradlePlugin {
         }
     }
 }
- */
+
+val javadocJar by tasks.registering(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles java doc to jar"
+    archiveClassifier.set("javadoc")
+}
 
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/BernatCarbo/swift-klib-plugin-fork")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+    publications.withType<MavenPublication>().configureEach {
+        artifact(javadocJar)
+        groupId = "io.github.bernatcarbo"
+        version = "${project.version}"
+        pom {
+            name.set("Swift Klib Gradle Plugin")
+            inceptionYear.set("2024")
+            description.set("Swift Klib Gradle Plugin with watchosDeviceArm64 support")
+            url.set("https://github.com/BernatCarbo/swift-klib-plugin-fork")
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://github.com/ttypic/swift-klib-plugin/blob/main/LICENSE")
+                }
+            }
+            issueManagement {
+                system.set("Github")
+                url.set("https://github.com/BernatCarbo/swift-klib-plugin-fork/issues")
+            }
+            developers {
+                developer {
+                    id.set("bernatcarbo")
+                    name.set("bernatcarbo")
+                    url.set("https://github.com/BernatCarbo/swift-klib-plugin-fork")
+                }
+            }
+            scm {
+                url.set("https://github.com/BernatCarbo/swift-klib-plugin-fork")
+                connection.set("scm:git:git://github.com/BernatCarbo/swift-klib-plugin-fork.git")
+                developerConnection.set("scm:git:ssh://git@github.com/BernatCarbo/swift-klib-plugin-fork.git")
             }
         }
     }
+
+    val signingTasks = tasks.withType<Sign>()
+    tasks.withType<AbstractPublishToMaven>().configureEach {
+        mustRunAfter(signingTasks)
+    }
+}
+
+signing {
+    val signingKey = System.getenv("MAVEN_SIGN_KEY")
+    val signingPassword = System.getenv("MAVEN_SIGN_PASSWORD")
+
+    useInMemoryPgpKeys(null, signingKey, signingPassword)
+    sign(publishing.publications)
 }
