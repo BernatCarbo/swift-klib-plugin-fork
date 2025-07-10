6e379e168bd5f7b9e6e50735a72e9e046017c685
bernat <bernatgarciacarbo@gmail.com>
2025-07-10 22:36:52 +0200

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ group = "io.github.bernatcarbo"
 nexusPublishing {
     repositories {
         sonatype {
-            stagingProfileId = "io.github.bernatcarbo--5eccc0c4-ded7-496a-82dc-89a86f40e182"
             nexusUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
             username = System.getenv("MAVEN_USERNAME")
             password = System.getenv("MAVEN_TOKEN")
